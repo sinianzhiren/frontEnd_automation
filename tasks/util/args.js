47f84fb70da89012d2b18acf8195adec4017b3be
yangpeixiao <yangpeixiao@duxiaoman.com>
2019-07-20 13:02:49 +0800

    mac commit

--- tasks/util/args.js
+++ tasks/util/args.js
@@ -2,34 +2,34 @@ import yargs from 'yargs';
 
 const args = yargs
 
-  .option('production',{
-    boolean:true,
-    default:false,
-    describe:'min all scripts'
-  })
+	.option('production', {
+		boolean: true,
+		default: false,
+		describe: 'min all scripts'
+	})
 
-  .option('watch',{
-    boolean:true,
-    default:false,
-    describe:'watch all files'
-  })
+	.option('watch', {
+		boolean: true,
+		default: false,
+		describe: 'watch all files'
+	})
 
-  .option('verbose',{
-    boolean:true,
-    default:false,
-    describe:'log'
-  })
+	.option('verbose', {
+		boolean: true,
+		default: false,
+		describe: 'log'
+	})
 
-  .option('sourcemaps',{
-    describe:'force the creation of sroucemaps'
-  })
+	.option('sourcemaps', {
+		describe: 'force the creation of sroucemaps'
+	})
 
-  .option('port',{
-    string:true,
-    default:8080,
-    describe:'server port'
-  })
+	.option('port', {
+		string: true,
+		default: 4399,
+		describe: 'server port'
+	})
 
-  .argv
+	.argv
 
 export default args;
